37ba1611e68f1c016dd559752feaafa23752c6f0
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-21 00:52:29 +0200

    new build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
